111108360ed9ff4af2865c64ee6f0d2257399819
Ori Harel <oeharel@gmail.com>
2022-06-06 21:52:25 +0300

    added join command

---
+++ lib/command/leave_gc_command.ts
@@ -0,0 +1,18 @@
+import { proto, WASocket } from "@adiwajshing/baileys";
+import { ICommand } from "./core/command";
+
+export default class LeaveCommand extends ICommand {
+    command: string = "gtfo";
+    async execute(client: WASocket, message: proto.IWebMessageInfo) {
+        const groupMeta = await client.groupMetadata(message.key.remoteJid!);
+
+        for (const participant of groupMeta.participants) {
+            if (participant.id == message.key.id && (participant.isAdmin || participant.isSuperAdmin)) {
+                await client.sendMessage(message.key.remoteJid!, { text: "Leaving the group chat...\nPeace out ✌️" })
+                return await client.groupLeave(groupMeta.id);
+            }
+        }
+
+        client.sendMessage(message.key.remoteJid!, {text: "Only a group admin can run this command."})
+    }
+}
