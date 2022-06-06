6ac0338e4fd096bb11f3ce12ad89ebe612c0639f
Ori Harel <oeharel@gmail.com>
2022-06-06 18:49:15 +0300

    added join command

--- lib/command/join_command.ts
+++ lib/command/join_command.ts
@@ -2,20 +2,30 @@ import {
     proto,
     WASocket,
   } from "@adiwajshing/baileys";
-import { whatsappBot } from "..";
-import { prefix as bot_prefix } from "../config";
 import { getMessageBody } from "../utils/message_utils";
   import {ICommand} from "./core/command";
   
   export default class JoinCommand implements ICommand {
     command: string = "join";
+    groupInviteRegex: RegExp = RegExp(/(https?:\/\/)?chat\.whatsapp\.com\/(?:invite\/)?([a-zA-Z0-9_-]{22})/g)
+
     async execute(client: WASocket, message: proto.IWebMessageInfo) {
-        if (!message.message?.groupInviteMessage) {
-            return client.sendMessage(message.key.remoteJid!, {text:"You must add an group invite link to the message."}, {quoted: message})
+        const body = getMessageBody(message);
+        const matches = this.groupInviteRegex.exec(body ?? "");
+
+        if (!matches || (matches && matches.length < 1)) {
+            return client.sendMessage(message.key.remoteJid!, {text:"You must have one group invite link in the message."}, {quoted: message})
         }
 
-        await client.groupAcceptInviteV4(message.key.remoteJid!, message.message.groupInviteMessage!);
-        client.sendMessage(message.message.groupInviteMessage.groupJid!, {text: "**Disclaimer**\nThis bot is handled and managed by Ori Harel.\nAs such, he poses the ability to see the messages in this group chat.\nHe does not plan to but the possibility is there. If you are not keen with this, please remove the bot."})
+        const code = matches[2];
+        try{
+            const res = await client.groupAcceptInvite(code);
+            client.sendMessage(message.key.remoteJid!, {text:"Joining the group..."}, {quoted: message})
+            client.sendMessage(res, {text: "**Disclaimer**\nThis bot is handled and managed by Ori Harel.\nAs such, he poses the ability to see the messages in this group chat.\nHe does not plan to but the possibility is there.\nIf you are not keen with this, please remove the bot."})
+
+        } catch (e) {
+            client.sendMessage(message.key.remoteJid!, {text:"Failed to join group.\nUnauthorized."}, {quoted: message})
+        }
     }
   }
   
