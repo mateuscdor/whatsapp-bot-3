83ad75e9721f63779563debb1e72fb5957cbdc79
Heknon <development@heknon.com>
2022-06-10 20:59:29 +0300

    damn whatsapp fonts

--- lib/command/help_command.ts
+++ lib/command/help_command.ts
@@ -10,7 +10,7 @@ export default class HelpCommand extends ICommand {
         let helpMessage = "*----- HELP ME I'M RETARDED ----*\n>>help - Sends this message\n>>music (song name) - Downloads the song from YouTube\n>>sticker - Send along with an image or video to create a sticker\n"
         helpMessage += ">>code - The code to this bot\n>>random folder_name - Send a random picture of some dude\n@ everyone - (Without the space) Tag everyone in the group\n"
         helpMessage += ">>join <group link> - adds the bot to a group (you can send the bot a private message)\n"
-        helpMessage += '>>spoof <tag> "<spoofed message>" "<user message>"\n>>lmgtfy <query> - condescending way to explain Google to someone\n'
+        helpMessage += '>>spoof <tag> "<spoofed message>" "<user message>"\n>>Lmgtfy <query> - condescending way to explain Google to someone\n'
         helpMessage += '>>add - Reply to a message with a contact to add that contact\n>>kick @person @person2 - Kick people from group\n'
         helpMessage += 'מקווה שעזרתי ✌\n~bot'
 
